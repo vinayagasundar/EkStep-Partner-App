5c6ffff04c7025802cf3d0d927853cab71c90df7
Vinayaga Sundar <vinayaga.sundar@tarento.com>
2017-04-03 15:06:51 +0530

    feat : start genie app with the selected child

--- app/src/main/java/org/partner/fragment/ChildDetailFragment.java
+++ app/src/main/java/org/partner/fragment/ChildDetailFragment.java
@@ -1,6 +1,9 @@
 package org.partner.fragment;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,12 +18,23 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import org.ekstep.genieservices.sdks.Partner;
 import org.ekstep.genieservices.sdks.UserProfile;
 import org.ekstep.genieservices.sdks.response.GenieListResponse;
+import org.ekstep.genieservices.sdks.response.GenieResponse;
 import org.json.JSONArray;
 import org.partner.BuildConfig;
 import org.partner.R;
+import org.partner.Util.AppConstants;
+import org.partner.Util.TelemetryEventGenertor;
+import org.partner.Util.Util;
+import org.partner.callback.CurrentGetuserResponseHandler;
+import org.partner.callback.CurrentuserResponseHandler;
+import org.partner.callback.ICurrentGetUser;
+import org.partner.callback.ICurrentUser;
+import org.partner.callback.IStartSession;
 import org.partner.callback.IUserProfile;
+import org.partner.callback.StartSessionResponseHandler;
 import org.partner.callback.UserProfileResponseHandler;
 import org.partner.model.Child;
 
@@ -31,7 +45,7 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class ChildDetailFragment extends Fragment
-        implements IUserProfile {
+        implements IUserProfile, IStartSession, ICurrentUser, ICurrentGetUser {
 
     private static final String TAG = "ChildDetailFragment";
 
@@ -53,6 +67,10 @@ public class ChildDetailFragment extends Fragment
 
 
     private UserProfile mUserProfile;
+
+    private Partner mPartner;
+
+    private ProgressDialog mProgressDialog;
 
 
     /**
@@ -112,7 +130,7 @@ public class ChildDetailFragment extends Fragment
         launchGenie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processData();
             }
         });
 
@@ -157,8 +175,95 @@ public class ChildDetailFragment extends Fragment
 
         Toast.makeText(getActivity(), genieListResponse.getError(),
                 Toast.LENGTH_SHORT).show();
+
+        hideDialog();
     }
 
+
+    @Override
+    public void onSuccessSession(GenieResponse genieResponse) {
+        if (DEBUG) {
+            Log.i(TAG, "onSuccessSession: " + genieResponse);
+        }
+
+        CurrentuserResponseHandler responseHandler = new CurrentuserResponseHandler(this);
+        mUserProfile.setCurrentUser(mUid, responseHandler);
+    }
+
+    @Override
+    public void onFailureSession(GenieResponse genieResponse) {
+        if (DEBUG) {
+            Log.i(TAG, "onFailureSession: " + genieResponse.getError());
+        }
+
+        Toast.makeText(getActivity(), genieResponse.getError(), Toast.LENGTH_SHORT).show();
+
+        hideDialog();
+    }
+
+
+    @Override
+    public void onSuccessCurrentUser(GenieResponse genieResponse) {
+        if (DEBUG) {
+            Log.i(TAG, "onSuccessCurrentUser: " + genieResponse);
+        }
+
+        CurrentGetuserResponseHandler responseHandler = new CurrentGetuserResponseHandler(this);
+        mUserProfile.setCurrentUser(mUid, responseHandler);
+    }
+
+    @Override
+    public void onFailureCurrentUser(GenieResponse genieResponse) {
+        if (DEBUG) {
+            Log.i(TAG, "onFailureCurrentUser: " + genieResponse.getError());
+        }
+
+        Toast.makeText(getActivity(), genieResponse.getError(), Toast.LENGTH_SHORT).show();
+
+        hideDialog();
+    }
+
+
+    @Override
+    public void onSuccessCurrentGetUser(GenieResponse genieResponse) {
+        if (DEBUG) {
+            Log.i(TAG, "onSuccessCurrentGetUser: " + genieResponse);
+        }
+
+        Util util = (Util) getActivity().getApplication();
+
+        TelemetryEventGenertor.generateOEEndEvent(getActivity(), mUid, util.getStartTime(),
+                System.currentTimeMillis());
+
+
+        PackageManager manager = getActivity().getPackageManager();
+        try {
+            //Toast.makeText(MainActivity.this, packageName, Toast.LENGTH_LONG).show();
+            Intent intent = manager.getLaunchIntentForPackage(
+                    AppConstants.GENIE_SERVICES_PACKAGE_NAME);
+            if (intent == null) {
+                return;
+                //throw new PackageManager.NameNotFoundException();
+            }
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            getActivity().startActivity(intent);
+            getActivity().finish();
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public void onFailureCurrentGetUser(GenieResponse genieResponse) {
+        if (DEBUG) {
+            Log.i(TAG, "onFailureCurrentGetUser: " + genieResponse.getError());
+        }
+
+        Toast.makeText(getActivity(), genieResponse.getError(), Toast.LENGTH_SHORT).show();
+
+        hideDialog();
+    }
 
     private void displayChildDetails(Child child) {
         mHandleText.setText(child.getHandle());
@@ -166,5 +271,31 @@ public class ChildDetailFragment extends Fragment
         mAgeText.setText(String.valueOf(child.getAge()));
         mClassText.setText(String.valueOf(child.getStandard()));
         mGenderText.setText(child.getGender());
+    }
+
+
+    private void processData() {
+        showDialog();
+
+        mPartner = new Partner(getActivity());
+
+        StartSessionResponseHandler responseHandler = new StartSessionResponseHandler(this);
+        mPartner.startPartnerSession(AppConstants.PARTNER_ID, responseHandler);
+    }
+
+    private void showDialog() {
+        hideDialog();
+
+        mProgressDialog = new ProgressDialog(getActivity());
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setMessage("Please wait");
+        mProgressDialog.show();
+    }
+
+    private void hideDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+            mProgressDialog = null;
+        }
     }
 }
