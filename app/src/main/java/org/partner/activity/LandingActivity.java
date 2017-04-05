04c948ecfd750a7d6de310faa53b7f3864ab58ce
Vinayaga Sundar <vinayaga.sundar@tarento.com>
2017-04-05 15:34:26 +0530

    fix : reverted the deleted the files

--- app/src/main/java/org/partner/activity/LandingActivity.java
+++ app/src/main/java/org/partner/activity/LandingActivity.java
@@ -12,6 +12,7 @@ import org.partner.R;
 import org.partner.callback.LaunchFragmentCallback;
 import org.partner.fragment.ChildDetailFragment;
 import org.partner.fragment.CreateChildFragment;
+import org.partner.fragment.ListChildFragment;
 
 public class LandingActivity extends AppCompatActivity
         implements LaunchFragmentCallback {
